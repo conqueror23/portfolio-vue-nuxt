13b62a7a1a29139d6c531dcc9f867d476e548163
conqueror23 <32027885+conqueror23@users.noreply.github.com>
2019-07-24 21:27:34 +1000

    1st update

---
+++ components/layout/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <nuxt-link to="/">HOME</nuxt-link>
+        <nuxt-link to="projects">PROJECTS</nuxt-link>
+        <nuxt-link to="resume">RESUME</nuxt-link>
+        <nuxt-link to="contact">CONTACT</nuxt-link>
+
+
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/contact.vue
@@ -0,0 +1,15 @@
+<template>
+    <table id="contact">
+        
+    </table>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
