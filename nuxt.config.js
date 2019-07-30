7c5a70a14591779213db00a69f7553808ae04f29
conqueror23 <32027885+conqueror23@users.noreply.github.com>
2019-07-30 19:04:37 +1000

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,52 +1,45 @@
-
 export default {
-  mode: 'universal',
+  mode: "universal",
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || "",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
   /*
-  ** Global CSS
-  */
-  css: [
-    'ant-design-vue/dist/antd.css'
-  ],
+   ** Global CSS
+   */
+  css: ["ant-design-vue/dist/antd.css"],
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-    '@/plugins/antd-ui',
-    '@/plugins/vue-scrollto',
-    '@/plugins/axios',
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: ["@/plugins/antd-ui", "@/plugins/vue-scrollto", "@/plugins/axios"],
   /*
-  ** Nuxt.js modules
-  */
-  modules: [
-  ],
+   ** Nuxt.js modules
+   */
+  modules: [],
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
-    vendor:['axios'],
+    vendor: ["axios"],
     /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
-    }
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
   }
-}
+};
